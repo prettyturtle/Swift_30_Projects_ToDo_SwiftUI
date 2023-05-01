5c34f7c7e8227d4a50b7ac90da99afcc411d045b
prettyturtle <unifconti@gmail.com>
2023-05-01 12:50:52 +0900

    Initial Commit

---
+++ Swift_30_Projects_ToDo/Swift_30_Projects_ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_30_Projects_ToDoApp.swift
+//  Swift_30_Projects_ToDo
+//
+//  Created by yc on 2023/05/01.
+//
+
+import SwiftUI
+
+@main
+struct Swift_30_Projects_ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
